95b3748732b670f2d8681f1f455259f80889cc9b
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-01-11 17:11:52 -0800

    Rendering of face box more accurate

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,22 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:ui' as ui;
 
+import 'dart:math' show max;
+
 import 'package:flutter/material.dart';
 
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -70,58 +69,81 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+/// Displays an image with faces highlighted
 class ImageFaces extends StatelessWidget {
   ImageFaces({this.imageFile, this.faces});
   final File imageFile;
   final List<Face> faces;
 
-  List<Widget> getFaceBoxes(double ratio) {
+  List<Widget> _getFaceBoxes(Size originalImageSize, Size newImageSize) {
     final faceBoundingBoxes = <Widget>[];
-    for (var face in faces) {
+    faces.forEach((face) {
       print('Face detected: ${face.boundingBox}');
       print(
         'Face - top: ${face.boundingBox.top}, bottom: ${face.boundingBox.bottom}, left: ${face.boundingBox.left}, right: ${face.boundingBox.right}',
       );
-      final top = face.boundingBox.top * ratio;
-      final bottom = face.boundingBox.bottom * ratio;
-      final left = face.boundingBox.left * ratio;
-      final right = face.boundingBox.right * ratio;
+      final top = face.boundingBox.top *
+          (newImageSize.height / originalImageSize.height);
+      final bottom = face.boundingBox.bottom *
+          (newImageSize.height / originalImageSize.height);
+      final left = face.boundingBox.left *
+          (newImageSize.width / originalImageSize.width);
+      final right = face.boundingBox.right *
+          (newImageSize.width / originalImageSize.width);
 
       print(
-        'Face Scaled - top: $top, bottom: $bottom, left: $left, right: $right',
+        'Face box - top: $top, bottom: $bottom, left: $left, right: $right',
       );
 
+      final center =
+          Offset(((right - left) / 2) + left, ((bottom - top) / 2) + top);
+      final radius = [bottom - top, right - left].reduce(max) / 2;
+
+      print('Face center: $center, radius: $radius');
+      /*
       faceBoundingBoxes.add(Positioned(
-        top: top,
-        bottom: bottom,
-        left: left,
-        right: 265.0,
-        child: Container(color: Colors.green),
-      ));
-    }
+          top: top,
+          height: bottom - top,
+          left: left,
+          width: right - left,
+          child: Container(color: Colors.blue)));
+      */
+      faceBoundingBoxes.add(Circle(center: center, radius: radius));
+    });
+
     return faceBoundingBoxes;
   }
 
   @override
   Widget build(BuildContext context) {
-    final image = Image.file(imageFile);
+    final image = Image.file(imageFile, fit: BoxFit.contain);
     final completer = Completer<ui.Image>();
 
     image.image.resolve(ImageConfiguration()).addListener(
         (ImageInfo info, bool _) => completer.complete(info.image));
-
-    final screenSize = MediaQuery.of(context).size;
 
     return FutureBuilder(
         future: completer.future,
         builder: (context, AsyncSnapshot<ui.Image> snapshot) {
           if (snapshot.hasData) {
             print('Image size: ${snapshot.data}');
-            final imageRatio = screenSize.width / snapshot.data.width;
-            print('Ratio: $imageRatio');
-            print('Screen size: $screenSize');
-            final children = <Widget>[image]..addAll(getFaceBoxes(imageRatio));
-            return Stack(children: children);
+            return LayoutBuilder(builder: (context, constraints) {
+              print('Stack constraints: $constraints');
+              final faceBoxes = _getFaceBoxes(
+                Size(snapshot.data.width.toDouble(),
+                    snapshot.data.height.toDouble()),
+                Size(constraints.maxWidth, constraints.maxHeight),
+              );
+              return Stack(
+                children: [
+                  Positioned.fill(
+                      child: LayoutBuilder(builder: (context, constraints) {
+                    print('Image constraints: $constraints');
+                    return image;
+                  })),
+                ]..addAll(faceBoxes),
+              );
+            });
           }
           return Center(child: CircularProgressIndicator());
         });
@@ -133,4 +155,38 @@ class NoImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(child: Text('Please select an image'));
   }
+}
+
+class Circle extends StatelessWidget {
+  Circle({@required this.center, @required this.radius, this.child});
+  final Offset center;
+  final double radius;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: CirclePainter(center, radius),
+      child: Container(color: Colors.transparent),
+    );
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  CirclePainter(this.center, this.radius);
+  final Offset center;
+  final double radius;
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    print('Paint size: $size');
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10;
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) => false;
 }
